da05de9eead9dfedd9552f4bdb4229984404e58c
eth-button <eth-button@users.noreply.github.com>
2018-01-21 16:51:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# eth-send
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+An extremely compact library for sending Ethereum.
+
+## `ethSend`
+
+`ethSend` is has a very simple API, that manages creating and sending a transaction on the Ethereum network (via MetaMask or Mist).
+
+`ethSend(rpc, to, value, options)`
+
+* `rpc` - An instance of a `web3` or compatible rpc client.
+* `to` - The destination address.  This should be a properly formatted Ethereum address, with leading `0x`
+  * Example: `0xd2f4668D0e752e95a8CE01014233458471DDbA4B`
+* `value` - The value to send, in wei.  This should be either a `String`, a `Number`, or a [`BigNumber`](https://github.com/MikeMcl/bignumber.js/)
+  * If this is a string, it should be converted to hexadecimal (base 16) and have a leading `0x`
+* `options` - A hash of extra options for the transaction.
+  * `options.gas` - The default amount of gas to send with the transaction.  For a simple send, this defaults to `21000`
+  * `options.gasPrice` - The default price to pay for gas.  When using Mist or MetaMask, the user should have the option of overriding this.
+  * `options.data` - *(advanced usage)* Data to send with the transaction.  When interacting with a contract, this contains the information necessary to call functions and pass information to the contract.  For simple sends (sends to basic addresses), this data is ignored.  See [web3#sendTranaction](https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethsendtransaction) for more information.
+
+## Example Usage
+
+```
+import ethSend from 'eth-send';
+import Web3 from 'web3';
+import BigNumber from "bignumber.js";
+
+const web3 = new Web3(window.web3.currentProvider);
+const ETHER = new BigNumber("1000000000000000000");
+
+ethSend(
+  web3,
+  '0xd2f4668D0e752e95a8CE01014233458471DDbA4B',
+  new BigNumber('0.5')).times(ETHER).floor(),
+  {
+    gas: '21000',
+    gasPrice: '1000000000',
+    data: 'Donation!'
+  }
+);
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+function getAccounts(rpc) {
+  return rpc.sendAsync({
+    method: "eth_accounts"
+  });
+}
+
+function sendTransaction(rpc, from, to, value, options) {
+  options = options || {};
+  const params = {
+    from: from,
+    to: to,
+    value: value
+  };
+  if (options.gas) {
+    params.gas = options.gas;
+  }
+  if (options.gasPrice) {
+    params.gasPrice = options.gasPrice;
+  }
+  if (options.data) {
+    if (typeof options.data === "function") {
+      params.data = options.data();
+    } else {
+      params.data = options.data;
+    }
+  }
+  return rpc.sendAsync({
+    method: "eth_sendTransaction",
+    params: [params]
+  });
+}
+
+function wait(delay) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
+function waitForTransaction(rpc, txid) {
+  return rpc
+    .sendAsync({
+      method: "eth_getTransactionReceipt",
+      params: [txid]
+    })
+    .then(function(res) {
+      if (!res) {
+        // poll every 1000 ms
+        // TODO: timeout?
+        return wait(1000).then(function() {
+          return waitForTransaction(rpc, txid);
+        });
+      } else {
+        return res;
+      }
+    });
+}
+
+/**
+ * Sends the specified amount of Ether from the primary account to the
+ * specified destination account.
+ *
+ * Additional options can be configured to provide better defaults to
+ * the underlying rpc.  These are generally treated as defaults--MetaMask,
+ * for example, provides the ability to override these when confirming
+ * the transaction.
+ *
+ * @param  {web3} rpc       An rpc provider, such as `web3` or `ethjs-rpc`
+ * @param  {Address} to     The destination account address.  Should always
+ *                          be a string with a leading `0x`
+ * @param  {mixed} value    The value, in wei, to send.  If this is a string,
+ *                          it should be in base 16 with a leading `0x`.
+ *                          Otherwise, this should be a `Number` or a `BigNumber`
+ * @param  {Object} options Additional options for customization:
+ * @param  {String} options.gas            Default amount of gas to provide
+ * @param  {String} options.gasPrice       Gas price to pay for the transaction
+ * @param  {String|Function} options.data  Data to include in the transaction.
+ *                                         If this is a function, this expects
+ *                                         the function to return a string
+ *                                         value.
+ * @return {Promise}        A Promise that resolves when the transaction has
+ *                          been mined, or rejects when any step in the process
+ *                          fails.  The resolved value will be the
+ *                          transaction's receipt.
+ */
+export default function ethSend(rpc, to, value, options) {
+  return getAccounts(rpc)
+    .then(function(accounts) {
+      return sendTransaction(rpc, accounts[0], to, value, options);
+    })
+    .then(function(txid) {
+      return waitForTransaction(rpc, txid);
+    });
+}
